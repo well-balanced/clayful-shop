e107ac509c6c415054fec69ce141fa25b5a9f4e1
Woosik Kim <iam.woosik.kim@gmail.com>
2021-09-03 12:05:32 +0900

    :art: Add RightButtons Component

---
+++ src/components/RightButtons/RightButtons.tsx
@@ -0,0 +1,13 @@
+import NextCutsomButtonLink from './ButtonLink'
+
+const RightButtons = () => {
+  return (
+    <div>
+      <NextCutsomButtonLink href="/login">로그인</NextCutsomButtonLink>
+      <NextCutsomButtonLink href="/signup">회원가입</NextCutsomButtonLink>
+      <NextCutsomButtonLink href="/order">주문내역</NextCutsomButtonLink>
+    </div>
+  )
+}
+
+export default RightButtons
