873403b545f42bf170c0e4ed49a169bfbc117c6f
Woosik Kim <iam.woosik.kim@gmail.com>
2021-09-03 12:41:45 +0900

    :sparkles: Add ClayfulFetcher

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,1 +1,2 @@
-export const NEXT_PUBLIC_API_URL = process.env.NEXT_PUBLIC_API_URL || ''
+export const CLAYFUL_API_URL = process.env.CLAYFUL_API_URL || ''
+export const CLAYFUL_ACCESS_TOKEN = process.env.CLAYFUL_ACCESS_TOKEN || ''
