68438a1de7ba48d15851741045eeb1578d8f88aa
well-balanced <iam.woosik.kim@gmail.com>
2021-09-13 15:10:17 +0900

    :alien: Add a cart API

---
+++ src/pages/api/cart/index.ts
@@ -0,0 +1,27 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+import clayfulPost from 'utils/clayfulPost'
+import { extractToken } from 'auth'
+import { CartItem, CartTotalPrice } from 'types'
+
+interface ClayfulPayload {
+  cart: {
+    items: CartItem[]
+    total: CartTotalPrice
+  }
+}
+
+interface Payload {
+  data?: ClayfulPayload
+  errorCode?: any
+  statusCode?: any
+}
+
+async function handler(req: NextApiRequest, res: NextApiResponse<Payload>) {
+  const token = extractToken(req)
+  const payload = await clayfulPost<ClayfulPayload>('/me/cart', req.body, {
+    headers: { 'Authorization-Customer': token },
+  })
+  res.status(payload.statusCode).json(payload)
+}
+
+export default handler
